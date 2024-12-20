8a9fcedeb8265ae839383370e49c405f1af8e4e2
Douglas <douglas.ofc.2004@gmail.com>
2024-12-19 22:26:13 -0300

    Finaliza o endpoint de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,24 @@
 import database from "infra/database.js";
+import { version } from "react";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum; ");
-  console.log(result.rows);
-  response
-    .status(200)
-    .json({ chave: "alunos do curso.dev são pessoas acima da média" });
-}
+  const databaseName = process.env.POSTGRES_DB;
+  const conexao_usada = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
 
+  const updateAT = new Date().toISOString();
+
+  const dbversion = await database.query("SHOW server_version;");
+
+  const maxconexao = await database.query("SHOW max_connections;");
+
+  response.status(200).json({
+    update_at: updateAT,
+    db_version: dbversion.rows[0].server_version,
+    max_connections: parseInt(maxconexao.rows[0].max_connections),
+    active_connections: conexao_usada.rows[0].count,
+  });
+}
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,23 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  // Testa o Json com a data da resposta;
+  const responseBody = await response.json();
+
+  const at_date_response = new Date(responseBody.update_at).toISOString;
+  const at_date = new Date().toISOString;
+  expect(at_date_response).toBe(at_date);
+
+  // Testa a resposta da Versão atual do Postgres;
+  expect(responseBody.db_version).toBeDefined();
+  expect(responseBody.db_version).toBe("16.0");
+
+  // Testa a resposta do max de conexões;
+  expect(responseBody.max_connections).toBeDefined();
+  expect(responseBody.max_connections).toBe(100);
+
+  // Testa a resposta da QTD de conexões usadas;
+  expect(responseBody.active_connections).toBeDefined();
+  expect(responseBody.active_connections).toBe(1);
 });
